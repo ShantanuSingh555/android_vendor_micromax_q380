45073eea9e81c59f62a612114df7849211c98864
ShankarWarang <shankar.warang9@gmail.com>
2017-01-04 20:14:12 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Vendor Blobs for Micromax Canvas Spark Q380
 ============================================
 * Check device tree here:
 https://github.com/ShankarWarang/android_device_micromax_q380.git
+
+
---
+++ q380-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q380/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q380/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q380/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q380/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q380/proprietary/vendor,system/vendor)
